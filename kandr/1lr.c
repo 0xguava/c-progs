38bc54706dc6d8a58e1bc03e54abde3e0aa6b137
0xguava <demo.lappy@protonmail.com>
2025-03-09 12:30:14 +0530

    lol321 cidr

--- kandr/1lr.c
+++ kandr/1lr.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
+int getl(char line[], int maxlines);
+void cpy(char from[], char to[]);
+
 void main(){
   /* ============== 1.6 ============== */
   /* int c, i, nwh, noth; */
@@ -22,4 +25,44 @@ void main(){
   /* for(i=0;i<10;i++) */
   /*   printf(" %d", ndigi[i]); */
   /* printf(", whites = %d and others = %d", nwh, noth); */
+
+  /* ============== 1.6 ============== */
+  int slen, len, max=0;
+  char line[1000], longest[1000];
+
+  while((len = getl(line,1000)) > 0){
+    if(len > max){
+      max = slen = len;
+      cpy(line, longest);
+    }
+  }
+
+  printf("length of longest: %d\n",slen);
+
+  if(max != 0)
+    printf("%s",longest);
+  
+}
+
+
+/* ============== 1.6 ============== */
+int getl(char line[], int maxline){
+  int c,i;
+  for(i=0;i<maxline-1 && (c = getchar()) != EOF && c != '\n'; i++)
+    line[i] = c;    
+
+  if(c == '\n'){
+    line[i] = c;
+    i++;
+  }
+  line[i] = '\0';
+
+  return i;
+}
+
+
+void cpy(char from[], char to[]){
+  int i=0;
+  while((to[i] = from[i]) != '\0')
+    i++;
 }
